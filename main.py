958b088b60e5dad03e3d4a65b636041e4261dd6d
agarret <aurelien.garret@capgemini.com>
2019-07-24 11:51:38 +0200

    Feature Albert Einstein

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 # Ajouter votre dependance ici :
 from aureliengarret import ag
 from thomasottone import to
+from alberteinstein import ae
 
 # Ajouter votre variable dans la liste ici :
 users = [
     ag, 
-    to
+    to,
+    ae
 ]
 
 
